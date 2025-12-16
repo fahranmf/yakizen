e9ce6386d7c9463c266add295d8bda6a0a4b2aef
fahranmf <fahranmaulana182@gmail.com>
2025-12-16 23:15:28 +0700

    add desc di menu

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -18,26 +18,98 @@ class MenuSeeder extends Seeder
 
         $menus = [
             // Ramen
-            ['category_id' => $ramen, 'name' => 'Shoyu Ramen', 'price' => 45000, 'image' => 'menus/01KC6MZERV486BJWR1MDY72K5B.jpg'],
-            ['category_id' => $ramen, 'name' => 'Miso Ramen', 'price' => 50000, 'image' => 'menus/01KC6N25QVAG146AJTSV4DAJFW.jpeg'],
-            ['category_id' => $ramen, 'name' => 'Tonkotsu Ramen', 'price' => 55000, 'image' => 'menus/01KC6N3D9ZKVBAS6EN5RKK06DJ.jpg'],
+            [
+                'category_id' => $ramen,
+                'name' => 'Shoyu Ramen',
+                'description' => 'Ramen kuah kecap asin khas Jepang dengan kaldu ayam yang gurih.',
+                'price' => 45000,
+                'image' => 'menus/01KC6MZERV486BJWR1MDY72K5B.jpg'
+            ],
+            [
+                'category_id' => $ramen,
+                'name' => 'Miso Ramen',
+                'description' => 'Ramen dengan kuah pasta miso yang kaya rasa dan sedikit creamy.',
+                'price' => 50000,
+                'image' => 'menus/01KC6N25QVAG146AJTSV4DAJFW.jpeg'
+            ],
+            [
+                'category_id' => $ramen,
+                'name' => 'Tonkotsu Ramen',
+                'description' => 'Ramen kuah tulang babi yang kental dan gurih, dimasak berjam-jam.',
+                'price' => 55000,
+                'image' => 'menus/01KC6N3D9ZKVBAS6EN5RKK06DJ.jpg'
+            ],
 
             // Donburi
-            ['category_id' => $donburi, 'name' => 'Gyudon', 'price' => 40000, 'image' => 'menus/01KC6NBTKF3Q4PCWKY0RQ1H93N.jpg'],
-            ['category_id' => $donburi, 'name' => 'Katsudon', 'price' => 45000, 'image' => 'menus/01KC6NDF7DPVXWM0FQTB10Y8BS.jpg'],
+            [
+                'category_id' => $donburi,
+                'name' => 'Gyudon',
+                'description' => 'Nasi dengan irisan daging sapi dan bawang bombay saus manis gurih.',
+                'price' => 40000,
+                'image' => 'menus/01KC6NBTKF3Q4PCWKY0RQ1H93N.jpg'
+            ],
+            [
+                'category_id' => $donburi,
+                'name' => 'Katsudon',
+                'description' => 'Nasi dengan ayam goreng tepung dan telur setengah matang.',
+                'price' => 45000,
+                'image' => 'menus/01KC6NDF7DPVXWM0FQTB10Y8BS.jpg'
+            ],
 
             // Side Dish
-            ['category_id' => $side, 'name' => 'Takoyaki 5 pcs', 'price' => 25000, 'image' => 'menus/01KC6NG7ED32ATE3145XEA7ER8.jpg'],
-            ['category_id' => $side, 'name' => 'Gyoza 6 pcs', 'price' => 30000, 'image' => 'menus/01KC6NHX2SYJYTN508JEYD7K09.jpg'],
+            [
+                'category_id' => $side,
+                'name' => 'Takoyaki 5 pcs',
+                'description' => 'Bola-bola tepung isi gurita dengan saus khas Jepang.',
+                'price' => 25000,
+                'image' => 'menus/01KC6NG7ED32ATE3145XEA7ER8.jpg'
+            ],
+            [
+                'category_id' => $side,
+                'name' => 'Gyoza 6 pcs',
+                'description' => 'Pangsit Jepang isi ayam dan sayur, digoreng hingga renyah.',
+                'price' => 30000,
+                'image' => 'menus/01KC6NHX2SYJYTN508JEYD7K09.jpg'
+            ],
 
             // Minuman
-            ['category_id' => $drink, 'name' => 'Ocha Panas', 'price' => 8000, 'image' => 'menus/01KC6NK993YERNG4HMKC4WQAF6.jpg'],
-            ['category_id' => $drink, 'name' => 'Ocha Dingin', 'price' => 10000, 'image' => 'menus/01KC6NNR3Z1RXM0XYZ0378ZV8W.jpg'],
-            ['category_id' => $drink, 'name' => 'Lemon Tea', 'price' => 15000, 'image' => 'menus/01KC6NQ0K4YYJBA5CV8W5AF0ZY.avif'],
+            [
+                'category_id' => $drink,
+                'name' => 'Ocha Panas',
+                'description' => 'Teh hijau Jepang hangat yang menyegarkan.',
+                'price' => 8000,
+                'image' => 'menus/01KC6NK993YERNG4HMKC4WQAF6.jpg'
+            ],
+            [
+                'category_id' => $drink,
+                'name' => 'Ocha Dingin',
+                'description' => 'Teh hijau Jepang dingin tanpa gula.',
+                'price' => 10000,
+                'image' => 'menus/01KC6NNR3Z1RXM0XYZ0378ZV8W.jpg'
+            ],
+            [
+                'category_id' => $drink,
+                'name' => 'Lemon Tea',
+                'description' => 'Teh segar dengan perasan lemon asli.',
+                'price' => 15000,
+                'image' => 'menus/01KC6NQ0K4YYJBA5CV8W5AF0ZY.avif'
+            ],
 
             // Dessert
-            ['category_id' => $dessert, 'name' => 'Mochi Ice Cream', 'price' => 20000, 'image' => 'menus/01KC6NRJABPE85D531CAWF57GD.jpg'],
-            ['category_id' => $dessert, 'name' => 'Dorayaki', 'price' => 18000, 'image' => 'menus/01KC6NTA9XRVH0YN0TRZANJVZX.jpg'],
+            [
+                'category_id' => $dessert,
+                'name' => 'Mochi Ice Cream',
+                'description' => 'Mochi lembut dengan isian es krim dingin.',
+                'price' => 20000,
+                'image' => 'menus/01KC6NRJABPE85D531CAWF57GD.jpg'
+            ],
+            [
+                'category_id' => $dessert,
+                'name' => 'Dorayaki',
+                'description' => 'Kue khas Jepang dengan isian kacang merah manis.',
+                'price' => 18000,
+                'image' => 'menus/01KC6NTA9XRVH0YN0TRZANJVZX.jpg'
+            ],
         ];
 
         foreach ($menus as $menu) {
